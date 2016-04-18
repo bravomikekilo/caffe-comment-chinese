92066e0cdef2b7efe87e4f5870a26d6bd71e530c
baomingkun <bmk1221@126.com>
2016-04-18 13:17:47 +0800

    完成基类Layer的实现的注释

--- src/caffe/layer.cpp
+++ src/caffe/layer.cpp
@@ -1,13 +1,15 @@
 #include <boost/thread.hpp>
 #include "caffe/layer.hpp"
-
+//基类Layer的实现
 namespace caffe {
 
+/**InitMutex() 初始化前向互斥体 */
 template <typename Dtype>
-void Layer<Dtype>::InitMutex() {
+void Layer<Dtype>::InitMutex() { 
   forward_mutex_.reset(new boost::mutex());
 }
 
+/**Lock() 前向互斥体上锁 */
 template <typename Dtype>
 void Layer<Dtype>::Lock() {
   if (IsShared()) {
@@ -15,6 +17,7 @@ void Layer<Dtype>::Lock() {
   }
 }
 
+/**Unlock() 前向互斥体解锁释放 */
 template <typename Dtype>
 void Layer<Dtype>::Unlock() {
   if (IsShared()) {
@@ -22,6 +25,6 @@ void Layer<Dtype>::Unlock() {
   }
 }
 
-INSTANTIATE_CLASS(Layer);
+INSTANTIATE_CLASS(Layer); //宏操作 将模板类Layer在float和double上实例化 也就是说 只有float和double的Layer
 
 }  // namespace caffe
